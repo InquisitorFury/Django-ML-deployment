bf86232dd0a5b91b6938364c8f9581749d53c595
InquisitorFury <studentaccahmed@gmail.com>
2024-06-10 02:42:04 +0100

    creating home page and linking it to the irisapp main.html

--- src/irisapp/views.py
+++ src/irisapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def predictor(request):
+    return render(request, 'main.html')
--- src/mlapp/views.py
+++ src/mlapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
