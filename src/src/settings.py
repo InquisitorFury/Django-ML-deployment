9bea605e4e0758cd130c5fcb4d513a31f5f11d8e
InquisitorFury <studentaccahmed@gmail.com>
2024-06-10 01:35:45 +0100

    setting up the base of the django workspace

--- src/src/settings.py
+++ src/src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'mlapp',#django app
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "src.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
